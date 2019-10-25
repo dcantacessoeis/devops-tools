692250876a6a10e198a6dedf4381e351b54dec2b
dcantacessoeis <dcantacesso@eagleinvsys.com>
2019-10-25 13:47:58 -0400

    refactored cleanup

--- git_daily_backups/main.py
+++ git_daily_backups/main.py
@@ -1,10 +1,10 @@
 
-import os
-import time
+import os, time
 
-myCmd = 'git clone https://github.com/eagleinvsys-sd/sd-core-17 .'
-newFld = 'F:/SD-CORE-17-BACKUPS/' + time.strftime("%m_%d_%Y_%M_%S")
+clone= 'git clone https://github.com/eagleinvsys-sd/sd-core-17 .'
+newFld = 'F:/SD-CORE-17-BACKUPS/' + time.strftime("%m_%d_%y_%M_%S")
+
 os.makedirs(newFld)
 os.chdir(newFld)
-#os.system('git-lfs install')
-os.system(myCmd)
+# os.system('git-lfs install')
+os.system(clone)
