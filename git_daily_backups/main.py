7d63df9b960bbc0816f8acc9cb5cd86cd17cde80
dcantacessoeis <dcantacesso@eagleinvsys.com>
2019-11-04 14:01:55 -0500

    Updated with comments

--- git_daily_backups/main.py
+++ git_daily_backups/main.py
@@ -1,14 +1,17 @@
 
 import os, time
 
-clone= 'git clone https://github.com/eagleinvsys-sd/sd-core-17 .'
-pull = 'git pull origin'
-newFld = 'F:/SD-CORE-17-BACKUPS/' + time.strftime("%m_%d_%y")
+# Variable Declaration #
+clone= 'git clone https://github.com/eagleinvsys-sd/sd-core-17 .' # Command to clone a specific remote repository #
+pull = 'git pull origin' # Command to update the existing repository #
+newFld = 'F:/SD-CORE-17-BACKUPS/' + time.strftime("%m_%d_%y") # Give path and create a folder with today's date #
 
+# Check if a backup for today exists and if so perform a Pull to update the repo #
 if os.path.isdir(newFld):
 	print("Backup repository already exists.  Will perform a PULL")
 	os.chdir(newFld)
 	os.system(pull)
+# If backup for today doesn't exist then create a new forlder with today's date then clone the remote repository #
 else:
 	print("Backup repository doesn't exists.  Will perform a CLONE")
 	os.makedirs(newFld)
